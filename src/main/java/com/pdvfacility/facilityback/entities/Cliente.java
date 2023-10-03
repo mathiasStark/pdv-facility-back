689e325613d8d907f44b4bbf994b37ce1a9b9e51
mathiasStark <pgo.mathias@gmail.com>
2023-10-03 00:11:41 -0300

    feat: remove getters and setters methods, add lombok annotations

--- src/main/java/com/pdvfacility/facilityback/entities/Cliente.java
+++ src/main/java/com/pdvfacility/facilityback/entities/Cliente.java
@@ -1,5 +1,51 @@
 package com.pdvfacility.facilityback.entities;
 
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_cliente")
+@AllArgsConstructor
+@Getter
+@Setter
 public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String cpf;
+	private String endereco;
+	private String telefone;
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(cpf, other.cpf);
+	}
+	
+	
 
 }
